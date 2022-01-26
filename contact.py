8bae4d8d37c147f2d29e70319baedfcf004662c5
MBubnov <19lunafer97@gmail.com>
2022-01-26 12:01:46 +0300

    ДЗ № 3

---
+++ contact.py
@@ -0,0 +1,16 @@
+class Contact:
+
+    def __init__(self, name, middlename, lastname, homephone, mobile, workphone, faxphone, mail, mail2, mail3,
+                       byear, homephone2):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.homephone = homephone
+        self.mobile = mobile
+        self.workphone = workphone
+        self.faxphone = faxphone
+        self.mail = mail
+        self.mail2 = mail2
+        self.mail3 = mail3
+        self.byear = byear
+        self.homephone2 = homephone2
