19b4493a0aaf753078137945b2f07ac7f01e2c52
MBubnov <19lunafer97@gmail.com>
2022-01-28 16:24:39 +0300

    ДЗ №4

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+from group import Group
 
 class Application:
 
@@ -11,45 +15,45 @@ class Application:
 
     def logout(self):
         wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+        wd.find_element_by_link_text("Logout").click()
 
     def return_to_group_page(self):
         wd = self.wd
-        wd.find_element(By.LINK_TEXT, "group page").click()
+        wd.find_element_by_link_text("group page").click()
 
     def create_group(self, group):
         wd = self.wd
         self.open_groups_page()
         # init group creation
-        wd.find_element(By.LINK_TEXT, "new").click()
+        wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element(By.LINK_TEXT, "group_name").click()
-        wd.find_element(By.LINK_TEXT, "group_name").clear()
-        wd.find_element(By.LINK_TEXT, "group_name").send_keys(group.name)
-        wd.find_element(By.LINK_TEXT, "group_header").click()
-        wd.find_element(By.LINK_TEXT, "group_header").clear()
-        wd.find_element(By.LINK_TEXT, "group_header").send_keys(group.header)
-        wd.find_element(By.LINK_TEXT, "group_footer").click()
-        wd.find_element(By.LINK_TEXT, "group_footer").clear()
-        wd.find_element(By.LINK_TEXT, "group_footer").send_keys(group.footer)
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
-        wd.find_element(By.LINK_TEXT, "submit").click()
+        wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
     def open_groups_page(self):
         wd = self.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element_by_link_text("groups").click()
 
     def login(self, username, password):
         wd = self.wd
         self.open_home_page()
-        wd.find_element(By.LINK_TEXT, "user").click()
-        wd.find_element(By.LINK_TEXT, "user").clear()
-        wd.find_element(By.LINK_TEXT, "user").send_keys(username)
-        wd.find_element(By.LINK_TEXT, "pass").click()
-        wd.find_element(By.LINK_TEXT, "pass").clear()
-        wd.find_element(By.LINK_TEXT, "pass").send_keys(password)
-        wd.find_element(By.LINK_TEXT, "//input[@value='Login']").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self):
         wd = self.wd
