45990ef6b5bff4ac05439da737c3a6f724e959d4
MBubnov <19lunafer97@gmail.com>
2022-05-21 20:20:48 +0300

    Оптимизация ввода данных в формы для групп

--- test/test_edit_contact.py
+++
@@ -1,9 +0,0 @@
-from model.contact import Contact
-
-
-def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(Contact(name="th", middlename="hs", lastname="sfgdh", homephone="453", homephone2="33",
-                mobile="4", workphone="3", faxphone="333",
-                mail="125", mail2="578", mail3="457"))
-    app.session.logout()
