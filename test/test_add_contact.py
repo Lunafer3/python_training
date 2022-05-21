f148ab5bb7172994466a76a5fd0a8843d4e9cf79
MBubnov <19lunafer97@gmail.com>
2022-05-21 17:17:38 +0300

    удалил эмти

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from fixture.application import Application
+import pytest
 
+@pytest.fixture
+def app(request):
+        fixture = Application()
+        request.addfinalizer(fixture.destroy)
+        return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
@@ -8,14 +15,4 @@ def test_add_contact(app):
         Contact(name="имя", middlename="отчество", lastname="фамилия", homephone="111111", homephone2="999999",
                 mobile="88005553535", workphone="222222", faxphone="333333",
                 mail="intec@io.mail", mail2="intes@ru.sand", mail3="inter@gov.gav"))
-    app.return_to_home_page()
-    app.session.logout()
-
-def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create(
-            Contact(name="имя", middlename="отчество", lastname="фамилия", homephone="111111", homephone2="999999",
-                    mobile="88005553535", workphone="222222", faxphone="333333",
-                    mail="intec@io.mail", mail2="intes@ru.sand", mail3="inter@gov.gav"))
-    app.return_to_home_page()
     app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,8 +9,3 @@ def test_add_group(app):
     app.group.create(Group(name="123", header="123", footer="12345"))
     app.session.logout()
 
-def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
-
