ed4f95d9c6e5958469d1d3d8ed59b53f773373ce
MBubnov <19lunafer97@gmail.com>
2022-05-21 16:49:08 +0300

    DZ 6

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from fixture.application import Application
-import pytest
 
-@pytest.fixture
-def app(request):
-        fixture = Application()
-        request.addfinalizer(fixture.destroy)
-        return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
@@ -20,7 +13,7 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create_contact(
+    app.contact.create(
             Contact(name="имя", middlename="отчество", lastname="фамилия", homephone="111111", homephone2="999999",
                     mobile="88005553535", workphone="222222", faxphone="333333",
                     mail="intec@io.mail", mail2="intes@ru.sand", mail3="inter@gov.gav"))
