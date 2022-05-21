2975aeb0346384ece9da3fb3fe194c110e5b22f9
MBubnov <19lunafer97@gmail.com>
2022-05-21 14:59:31 +0300

    DZ 4  corrected

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+
+    def __init__(self, name, middlename, lastname, homephone, homephone2, mobile, workphone, faxphone, mail, mail2, mail3):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.homephone = homephone
+        self.homephone2 = homephone2
+        self.mobile = mobile
+        self.workphone = workphone
+        self.faxphone = faxphone
+        self.mail = mail
+        self.mail2 = mail2
+        self.mail3 = mail3
