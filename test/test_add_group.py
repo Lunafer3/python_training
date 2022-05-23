73496d2ac759a6b38a7be2e8c7cb2978bd85b175
MBubnov <19lunafer97@gmail.com>
2022-05-23 11:26:36 +0300

    домашняя работа №6

---
+++ test/test_add_group.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application import Application
+from conftest import app
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="123", header="123", footer="12345"))
+    app.session.logout()
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
+
