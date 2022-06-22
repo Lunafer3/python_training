089086674ef83a22f4ad230e4cd022e4dc30fd9d
Mikhail Bubnov <19lunafer97@gmail.com>
2022-06-22 22:00:25 +0300

    29 dz

--- fixture/application.py
+++ fixture/application.py
@@ -2,26 +2,23 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
-from fixture.orm import ORMFixture
 
 
 class Application:
-
-    def __init__(self, browser, base_url):
-        if browser == "firefox":
+    def __init__(self, browser, base_url, base_password):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+        elif browser == "firefox":
             self.wd = webdriver.Firefox()
-        elif browser == "chrome":
-            self.wd = webdriver.Chrome()
         elif browser == "ie":
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
-        self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.orm = ORMFixture(host='127.0.0.1', name='addressbook', user='root', password='')
+        self.base_password = base_password
 
     def is_valid(self):
         try:
--- test/test_home_page.py
+++ test/test_home_page.py
@@ -1,20 +1,51 @@
-from model.contact import Contact
-from random import randrange
 import re
+from random import randrange
+
+from model.contact import Contact
 
 
-def test_alldata_on_home_page(app, db):
-    contacts_from_home_page = app.contact.get_contact_list()
+def test_name_on_home_page(app):
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
 
-    for ui_contact in contacts_from_home_page:
-        db_contact = db.get_contact_by_id(ui_contact.id)
-        assert ui_contact.first_name == db_contact.first_name
-        assert ui_contact.last_name == db_contact.last_name
-        assert ui_contact.address == db_contact.address
-        assert ui_contact.all_phones_from_home_page == merge_phones_like_on_home_page(
-            db_contact)
-        assert ui_contact.all_emails_from_home_page == merge_emails_like_on_home_page(
-            db_contact)
+
+def test_addres_on_home_page(app):
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.address == contact_from_edit_page.address
+
+
+def test_emails_on_home_page(app):
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def test_phones_on_home_page(app):
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def test_phones_on_contact_view_page(app):
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_view_page.home == contact_from_edit_page.home
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_view_page.work == contact_from_edit_page.work
+    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
 
 def clear(s):
@@ -23,12 +54,34 @@ def clear(s):
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.homephone, contact.mobilephone, contact.workphone, contact.secondhomephone]))))
+                            map(lambda x: clear(x), filter(lambda x: x is not None,
+                                                           [contact.home,
+                                                            contact.mobile, contact.work,
+                                                            contact.phone2]))))
 
 
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            filter(lambda x: x is not None,
-                                   [contact.email, contact.email2, contact.email3])))
+    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, [contact.email,
+                                                                                contact.email2, contact.email3])))
+
+
+def test_all_contact_home_page_db(app, db, check_ui):
+    db_contacts = db.get_contact_list()
+    db_contacts = sorted(db_contacts, key=Contact.id_or_max)
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    assert len(db_contacts) == len(contacts_from_home_page)
+    assert db_contacts == contacts_from_home_page
+    for number in db_contacts:
+        number.all_emails_from_home_page = merge_emails_like_on_home_page(number)
+        number.all_phones_from_home_page = merge_phones_like_on_home_page(number)
+    for i in range(len(db_contacts)):
+        assert db_contacts[i].id == contacts_from_home_page[i].id
+        assert db_contacts[i].firstname == contacts_from_home_page[i].firstname
+        assert db_contacts[i].lastname == contacts_from_home_page[i].lastname
+        assert db_contacts[i].address == contacts_from_home_page[i].address
+        assert db_contacts[i].all_phones_from_home_page == contacts_from_home_page[i].all_phones_from_home_page
+        assert db_contacts[i].all_emails_from_home_page == contacts_from_home_page[i].all_emails_from_home_page
+        print(str(i))
+        print(db_contacts[i])
+        print(contacts_from_home_page[i])
+
